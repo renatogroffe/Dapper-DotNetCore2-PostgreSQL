1f3198710d38306505916a565d34c5ebe5b7da3e
Renato Groffe <renato.groffe@yahoo.com.br>
2017-09-30 00:24:41 -0300

    Dapper.Contrib + PostgreSQL + ASP.NET Core 2.0
    API REST utilizando Dapper.Contrib, PostgreSQL e ASP.NET Core 2.0

---
+++ Dapper_PostgreSQL/Scripts/Script-Indicadores.sql
@@ -0,0 +1,44 @@
+﻿CREATE TABLE Indicadores(
+	Sigla varchar(10) NOT NULL,
+	NomeIndicador varchar(60) NOT NULL,
+	UltimaAtualizacao date NOT NULL,
+	Valor numeric (18,4) NOT NULL,
+	CONSTRAINT PK_Indicadores PRIMARY KEY (Sigla)
+);
+
+
+
+INSERT INTO Indicadores
+           (Sigla
+           ,NomeIndicador
+           ,UltimaAtualizacao
+           ,Valor)
+     VALUES
+           ('SALARIO'
+           ,'Salario minimo'
+           ,'2017-01-01'
+           ,937.00);
+
+
+INSERT INTO Indicadores
+           (Sigla
+           ,NomeIndicador
+           ,UltimaAtualizacao
+           ,Valor)
+     VALUES
+           ('IPCA'
+           ,'Indice Nacional de Precos ao Consumidor Amplo'
+           ,'2017-08-31'
+           ,0.0019);
+
+
+INSERT INTO Indicadores
+           (Sigla
+           ,NomeIndicador
+           ,UltimaAtualizacao
+           ,Valor)
+     VALUES
+           ('SELIC'
+           ,'Taxa Referencial - Sistema de Liquidacao e Custodia'
+           ,'2017-09-06'
+           ,0.0825);
